51fd927ccb173a4dbd60237525e901f0aeb99a2f
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-11-10 19:14:15 -0500

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,11 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+<pre>
+  <code>
+    <h1 className="hero__title">NPM I {siteConfig.title}</h1>
+  </code>
+</pre>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
