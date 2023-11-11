65d25ac3a2e121fa9b58e6f1883c8ccf8c71087e
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-11-11 10:53:29 -0500

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\calvi\\Downloads\\kick-auth\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\calvi\\Downloads\\kick-auth\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\calvi\\Downloads\\kick-auth\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\calvi\\Downloads\\kick-auth\\my-website\\src\\css\\custom.css'),
-];
